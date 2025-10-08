91df4435aba32bf9d2f9a220e661f3bfc6c26a2a
thateflondon <jfpl.dev@gmail.com>
2025-10-08 13:18:51 +0200

    Update home

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,8 @@
 import type { Route } from "./+types/home";
 import Navbar from "~/components/Navbar";
+import {resumes} from "~/constants";
+import {resume} from "react-dom/server";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -10,12 +13,23 @@ export function meta({}: Route.MetaArgs) {
 
 export default function Home() {
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
-      <Navbar />
+      {/*Navbar*/}
+      <Navbar/>
+      {/*Navbar End */}
     <section className="main-section">
         <div className="page-heading">
             <h1>Track your applications and Resume Ratings</h1>
             <h2>Review your submissions and check AI-powered feedback</h2>
         </div>
     </section>
+
+      {/*map an array that contains different kind of resume && render if only the is minimum one resume*/}
+      {resumes.length > 0 && (
+          <div className="resumes-section">
+              {resumes.map((resume) => (
+                  <ResumeCard key={resume.id} resume={resume}/>
+              ))}
+          </div>
+      )}
   </main>
 }
