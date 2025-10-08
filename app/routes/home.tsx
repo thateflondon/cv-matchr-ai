fd6a3dcf2daf697483deaff4176cb673a4388b84
thateflondon <jfpl.dev@gmail.com>
2025-10-08 18:29:58 +0200

    set homepage

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -17,19 +17,23 @@ export default function Home() {
       <Navbar/>
       {/*Navbar End */}
     <section className="main-section">
-        <div className="page-heading">
+        {/*Header*/}
+        <div className="page-heading py-16">
             <h1>Track your applications and Resume Ratings</h1>
             <h2>Review your submissions and check AI-powered feedback</h2>
         </div>
-    </section>
+        {/*Header End*/}
 
-      {/*map an array that contains different kind of resume && render if only the is minimum one resume*/}
-      {resumes.length > 0 && (
-          <div className="resumes-section">
-              {resumes.map((resume) => (
-                  <ResumeCard key={resume.id} resume={resume}/>
-              ))}
-          </div>
-      )}
+        {/*Resume section*/}
+        {/*map an array that contains different kind of resume && render if only the is minimum one resume*/}
+        {resumes.length > 0 && (
+            <div className="resumes-section">
+                {resumes.map((resume) => (
+                    <ResumeCard key={resume.id} resume={resume}/>
+                ))}
+            </div>
+        )}
+        {/*Resume section end*/}
+    </section>
   </main>
 }
