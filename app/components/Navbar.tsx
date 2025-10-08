53a729c3a5894472ef65c1e92ad86b8b23141f81
thateflondon <jfpl.dev@gmail.com>
2025-10-08 03:48:26 +0200

    Navbar

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import {Link} from "react-router";
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <Link to="/">
+                <p className="text-2xl font-bold tetx-gradient">CV Match AI</p>
+            </Link>
+            <Link to="/upload">
+                <p className="primary-button w-fit">Upload Resume</p>
+            </Link>
+        </nav>
+    )
+}
+
+export default Navbar;
