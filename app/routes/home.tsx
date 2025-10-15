de9ebc97c7e6f0f32d450b26a8d886501b7cab98
thateflondon <jfpl.dev@gmail.com>
2025-10-15 23:55:31 +0200

    fetch user uploaded resume on home page

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,10 +1,9 @@
 import type { Route } from "./+types/home";
 import Navbar from "~/components/Navbar";
-import {resumes} from "~/constants";
 import ResumeCard from "~/components/ResumeCard";
 import {usePuterStore} from "~/lib/puter";
 import {useNavigate} from "react-router";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -14,14 +13,35 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-    const { auth } = usePuterStore(); // Vérifie l'état de chargement et l'authentification
+    const { auth, kv } = usePuterStore(); // Vérifie l'état de chargement et l'authentification
     const navigate = useNavigate(); // permet de naviguer vers la prochaine page
+    // const [resumeUrl, setResumeUrl] = useState("");
+    const [resumes, setResumes] = useState<Resume[]>([]);
+    const [loadingResumes, setLoadingResumes] = useState(false);
 
     useEffect(() => {
         if(!auth.isAuthenticated) navigate('/auth?next=/'); // Si pas authentification on affiche la page login
     }, [auth.isAuthenticated]);
 
-  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    useEffect(() => {
+        const loadResumes = async () => {
+            setLoadingResumes(true);
+
+            const resumes = (await kv.list('resume:*', true)) as KVItem[];
+
+            const parsedResumes = resumes?.map((resume) => (
+                JSON.parse(resume.value) as Resume
+            ))
+
+            console.log("parsedResumes", parsedResumes);
+            setResumes(parsedResumes || []);
+            setLoadingResumes(false);
+        }
+        loadResumes();
+
+    }, []);
+
+  return (<main className="bg-[url('/images/bg-main.svg')] bg-cover">
       {/*Navbar*/}
       <Navbar/>
       {/*Navbar End */}
@@ -29,13 +49,17 @@ export default function Home() {
         {/*Header*/}
         <div className="page-heading py-16">
             <h1>Track your applications and Resume Ratings</h1>
-            <h2>Review your submissions and check AI-powered feedback</h2>
+            {!loadingResumes && resumes?.length === 0 ? (
+                <h2>No resumes found. Upload your first resume to get feedback.</h2>
+            ): (
+                <h2>Review your submissions and check AI-powered feedback.</h2>
+            )}
         </div>
         {/*Header End*/}
 
         {/*Resume section*/}
         {/*map an array that contains different kind of resume && render if only the is minimum one resume*/}
-        {resumes.length > 0 && (
+        {!loadingResumes && resumes.length > 0 && (
             <div className="resumes-section">
                 {resumes.map((resume) => (
                     <ResumeCard key={resume.id} resume={resume}/>
@@ -44,5 +68,5 @@ export default function Home() {
         )}
         {/*Resume section end*/}
     </section>
-  </main>
+  </main>)
 }
