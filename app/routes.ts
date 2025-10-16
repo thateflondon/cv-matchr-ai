3a680649bcf51024e72780f302aca7ddf5711f74
thateflondon <jfpl.dev@gmail.com>
2025-10-16 05:32:50 +0200

    Add delete functionality

--- app/routes.ts
+++ app/routes.ts
@@ -5,4 +5,5 @@ export default [
     route("/auth", "routes/auth.tsx"),
     route("/upload", "routes/upload.tsx"),
     route("/resume/:id", "routes/resume.tsx"),
+    route('delete', 'routes/delete.tsx'),
 ] satisfies RouteConfig;
