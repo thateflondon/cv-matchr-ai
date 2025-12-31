c6d9e4dc3972e62b920550b39e7f28f3da564023
thateflondon <jfpl.dev@gmail.com>
2025-12-31 21:08:56 +0100

    create footer component

---
+++ app/components/Footer.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="footer-container">
+      <div className="footer-content">
+        <a href="" className="gtu">
+          General Terms of Use
+        </a>
+        <a href="" className="privacy">
+          Privacy Policy
+        </a>
+        <a href="" className="contact">
+          Contact Us
+        </a>
+      </div>
+      <div className="copyright">© 2025 CV Match. All rights reserved</div>
+    </div>
+  );
+}
+
+export default Footer;
