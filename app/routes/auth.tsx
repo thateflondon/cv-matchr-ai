d88fb75859c721da7be4b2ae85dc17214ed64364
thateflondon <jfpl.dev@gmail.com>
2026-01-02 22:51:04 +0100

    add hero section

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -25,21 +25,12 @@ const Auth = () => {
 
   return (
     <>
-      {/* <Navbar /> */}
-      {/* <HeroSection /> */}
       <main id="app" className="bg-[url('/images/bg-large.svg')] bg-cover">
       <div className="app-container auth">
         <Navbar userName={userName} />
         <HeroSection />
         {/* <Footer/> */}
       </div>
-        {/* <div className="">
-          <div className="absolute top-0 left-0 w-full h-full opacity-30 pointer-events-none">
-            <div className="absolute top-20 left-10 w-72 h-72 bg-blue-400 rounded-full mix-blend-multiply filter blur-xl animate-blob"></div>
-            <div className="absolute top-40 right-10 w-72 h-72 bg-purple-400 rounded-full mix-blend-multiply filter blur-xl animate-blob animation-delay-2000"></div>
-            <div className="absolute bottom-20 left-1/3 w-72 h-72 bg-pink-400 rounded-full mix-blend-multiply filter blur-xl animate-blob animation-delay-4000"></div>
-          </div>
-        </div> */}
       </main>
       <Footer/>
     </>
