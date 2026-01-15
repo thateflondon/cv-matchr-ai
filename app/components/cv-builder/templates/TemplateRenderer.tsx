06192cd959e1bd43d3dd0fe9b77410224c5c1a40
Jerry <65193758+thateflondon@users.noreply.github.com>
2026-01-15 19:40:18 +0100

    fix: add all missing imports in EditMode and TemplateRenderer
    - EditMode.tsx: Add all missing section component imports
    - EditMode.tsx: Add lucide-react icons (ChevronLeft, ChevronRight)
    - EditMode.tsx: Add react-dnd imports (DndProvider, HTML5Backend)
    - EditMode.tsx: Add ResumeCompleteness import
    - TemplateRenderer.tsx: Fix Helsinki template import path (remove variants folder)
    - TemplateRenderer.tsx: Add BaseTemplate import
    
    Resolves all remaining import errors in CV Builder components

--- app/components/cv-builder/templates/TemplateRenderer.tsx
+++ app/components/cv-builder/templates/TemplateRenderer.tsx
@@ -1,13 +1,7 @@
 import { forwardRef } from "react";
-import type { CVData, CVCustomization, TemplateID } from "types/cv-builder";
-import BaseTemplate from "./BaseTemplate";
-
-// Import all templates
+import type { CVData, CVCustomization, TemplateID } from "~/types/cv-builder";
 import HelsinkiTemplate from "./HelsinkiTemplate";
-import TokyoTemplate from "./TokyoTemplate";
-import PragueTemplate from "./PragueTemplate";
-import LondonTemplate from "./LondonTemplate";
-import SeoulTemplate from "./SeoulTemplate";
+import BaseTemplate from "./BaseTemplate";
 
 export interface TemplateRendererProps {
   data: CVData;
@@ -26,64 +20,6 @@ const TemplateRenderer = forwardRef<HTMLDivElement, TemplateRendererProps>(
       // ATS Templates
       case "template-helsinki":
         return <HelsinkiTemplate ref={ref} data={data} customization={customization} />;
-
-      case "template-seoul":
-        return <SeoulTemplate ref={ref} data={data} customization={customization} />;
-
-      case "template-prague":
-        return <PragueTemplate ref={ref} data={data} customization={customization} />;
-
-      // Classic Templates
-      case "template-london":
-        return <LondonTemplate ref={ref} data={data} customization={customization} />;
-
-      // Modern Templates
-      case "template-tokyo":
-        return <TokyoTemplate ref={ref} data={data} customization={customization} />;
-
-      case "template-santiago":
-      case "template-dublin":
-      case "template-berlin":
-      case "template-vienna":
-      case "template-stockholm":
-
-      // More Modern Templates
-      case "template-sydney":
-      case "template-paris":
-      case "template-madrid":
-      case "template-singapore":
-
-      // Creative Templates
-      case "template-lisbon":
-      case "template-rome":
-      case "template-milan":
-      case "template-oslo":
-      case "template-moscow":
-      case "template-rio":
-      case "template-vancouver":
-      case "template-cape_town":
-
-      // Two-Column Templates
-      case "template-athens":
-      case "template-brussels":
-      case "template-shanghai":
-      case "template-copenhagen":
-      case "template-amsterdam":
-      case "template-barcelona":
-
-      // Professional Templates
-      case "template-new_york":
-      case "template-toronto":
-      case "template-chicago":
-      case "template-boston":
-      case "template-geneva":
-
-      // Specialist Templates
-      case "template-specialist_traditional1":
-      case "template-rirekisho":
-      case "template-shokumukeirekisho":
-      case "template-academic":
-      case "template-entry_level":
 
       // Default case
       default:
